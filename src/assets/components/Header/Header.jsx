8e730f3f1b39e4867ffa7860991da136306d2330
FlowerPunk1642 <rodionmal07@gmail.com>
2026-09-06 23:45:32 +0600

    Лабораторная №1: React + Vite, семантика, CSS-модули, карточки

---
+++ src/assets/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import styles from './Header.module.css';
+import Nav from '../Nav/Nav';
+import articles from '../../data/articles';
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <h1 className={styles.title}>React Blog</h1>
+          <span className={styles.count}>Статей: {articles.length}</span>
+        </div>
+        <Nav />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
